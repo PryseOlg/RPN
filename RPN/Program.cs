e58697c707d52c3c3828ced5c962a8b0fc7abb7c
PryseOlg <oleg.kuts.02@mail.ru>
2021-02-26 22:14:48 +0500

    Added Library

--- RPN/Program.cs
+++ RPN/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+using RPNLogic;
 
 namespace RPN
 {
@@ -9,53 +7,7 @@ namespace RPN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ToPrefix("2+5*7-4:4"));
-            
+            Console.WriteLine(NotationConverter.ToPostFix("5+2*7-4:4"));
         }
-        
-        static string ToPrefix(string expression)
-        {
-            List<string> result = new List<string>();
-            Stack<string> operators = new Stack<string>();
-            foreach (char symbol in expression)
-            {
-                if (char.IsDigit(symbol))
-                {
-                    
-                    result.Add(symbol.ToString());
-                }
-                else
-                {
-                    while ((operators.TryPeek(out string head)) && (IsLowerPriority(symbol, head[0])))
-                    {
-                       result.Add(operators.Pop());
-                    }
-                    operators.Push(symbol.ToString());
-                }
-            }
-            while (operators.Count != 0)
-            {
-                result.Add(operators.Pop());
-            }
-
-            return string.Join(" ", result);
-        }
-
-        static bool IsLowerPriority(char current, char last)
-        {
-            string[][] priorities = new string[][] {new string[] {"+", "-"}, new string[] {"*", ":"}};
-            int priorityCurrent = 0, priorityLast = 0;
-            for (int i = 0; i < priorities.Length; i++)
-            {
-                string[] operators = priorities[i];
-                if (operators.Contains(current.ToString()))
-                    priorityCurrent = i;
-                if (operators.Contains(last.ToString()))
-                    priorityLast = i;
-            }
-            return (priorityCurrent <= priorityLast);
-
-        }
-        
     }
 }
---
+++ RPNLogic/NotationConverter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RPNLogic
+{
+    public class NotationConverter
+    {
+        public static string ToPostFix(string expression)
+        {
+            List<string> result = new List<string>();
+            Stack<string> operators = new Stack<string>();
+            foreach (char symbol in expression)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    
+                    result.Add(symbol.ToString());
+                }
+                else
+                {
+                    while ((operators.TryPeek(out string head)) && (IsLowerPriority(symbol, head[0])))
+                    {
+                        result.Add(operators.Pop());
+                    }
+                    operators.Push(symbol.ToString());
+                }
+            }
+            while (operators.Count != 0)
+            {
+                result.Add(operators.Pop());
+            }
+
+            return string.Join(" ", result);
+        }
+        private static bool IsLowerPriority(char current, char last)
+        {
+            string[][] priorities = new string[][] {new string[] {"+", "-"}, new string[] {"*", ":"}};
+            int priorityCurrent = 0, priorityLast = 0;
+            for (int i = 0; i < priorities.Length; i++)
+            {
+                string[] operators = priorities[i];
+                if (operators.Contains(current.ToString()))
+                    priorityCurrent = i;
+                if (operators.Contains(last.ToString()))
+                    priorityLast = i;
+            }
+            return (priorityCurrent <= priorityLast);
+
+        }
+    }
+}
